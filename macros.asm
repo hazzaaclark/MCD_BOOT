8d36f59b6f2de2dad76efa93128ed1b7ae964d53
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-05-27 14:01:09 +0100

    added sub cpu status'

--- macros.asm
+++ macros.asm
@@ -63,8 +63,45 @@ SUB_RTS             EQU         $4E75
 SUB_RTE             EQU         $4E73
 SUB_JMP             EQU         $4EF9
 
-SUB_GET_WORD        EQU         $01
-SUB_GET_FILE_ID     EQU         $02
-SUB_LOAD_FILE_NAME  EQU         $03
-SUB_LOAD_DLL_NAME   EQU         $04
-SUB_PLAY_CDDA       EQU         $05
+;; ACCESS MODE 1 FROM THE CARTRIDGE SLOT TO READ THE CONTENTS
+;; OF A GENERIC CD
+
+SUB_GET_WORD            EQU         $01
+SUB_GET_FILE_ID         EQU         $02
+SUB_LOAD_FILE_NAME      EQU         $03
+SUB_LOAD_DLL_NAME       EQU         $04
+SUB_PLAY_CDDA_READ      EQU         $05
+SUB_PLAY_CDDA           EQU         $06
+SUB_PAUSE_CDDA          EQU         $07
+SUB_RESUME_CDDA         EQU         $08
+SUB_STOP_CDDA           EQU         $09
+SUB_FADE_CDDA           EQU         $0A
+SUB_SEEK_CDDA           EQU         $0B
+
+SUB_WORD_MODE_1_RAM             EQU             $000c0000
+SUB_WORD_MODE_2_RAM             EQU             $00080000
+
+SUB_MAIN_FLAG                   EQU             $FFFF800E
+SUB_SECOND_FLAG	                EQU	            $FFFF800F
+SUB_COMMON_0                    EQU	            $FFFF8010
+SUB_COMMON_2		            EQU	            $FFFF8012
+SUB_COMMON_4		            EQU	            $FFFF8014
+SUB_COMMON_6                    EQU	            $FFFF8016
+SUB_COMMON_8		            EQU	            $FFFF8018
+SUB_COMMON_10	                EQU	            $FFFF801A
+SUB_COMMON_12	                EQU	            $FFFF801C
+SUB_COMMON_14	                EQU             $FFFF801E
+SUB_STATUS_0		            EQU	            $FFFF8020
+SUB_STATUS_2		            EQU             $FFFF8022
+SUB_STATUS_4		            EQU             $FFFF8024
+SUB_STATUS_6		            EQU	            $FFFF8026
+SUB_STATUS_8		            EQU             $FFFF8028
+SUB_STATUS_10	                EQU             $FFFF802A
+SUB_STATUS_12	                EQU             $FFFF802C
+SUB_STATUS_14	                EQU             $FFFF02E
+SUB_PROC_RAM	                EQU             $00000000
+SUB_PROC_RAM_B0	                EQU	            SUB_PROC_RAM	
+SUB_PROC_RAM_B1	                EQU	            SUB_PROC_RAM+$20000
+SUB_PROC_RAM_B2	                EQU	            SUB_PROC_RAM+$40000
+SUB_PROC_RAM_B3	                EQU             SUB_PROC_RAM+$60000
+SUB_MEM	                        EQU	            $FFFF8003
