78e804f19c83825a297d51ecb6472f2af1b48fb2
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-05-28 20:48:36 +0100

    updated includes

--- main.asm
+++ main.asm
@@ -1,46 +1,46 @@
 ;--------------------------------------------------------
 ;           COPYRIGHT (C) HARRY CLARK 2024
 ;--------------------------------------------------------
-;--------------------------------------------------------
 ;           GENERIC SEGA MEGA CD LOADER
 ;--------------------------------------------------------
-;--------------------------------------------------------
-;          THIS FILE PERTAINS TOWARDS THE MAIN       
+;          THIS FILE PERTAINS TOWARDS THE MAIN
 ;              FUNCTIONALITY OF THE PROGRAM
 ;--------------------------------------------------------
 
+    include "macros.asm"
+    include "BIOS_inc.asm"
+
 CD_HEADER:
 
-DISC_TYPE:              DC.B            'SEGADISCSYSTEM  '
-VOLUME_NAME:            DC.B            'SEGACDGAME     ', 0
-VOLUME_SYSTEM:          DC.W            $100, $1
-SYSTEM_NAME:            DC.B            'SEGACDGAME     ', 0
-SYSTEM_VER:             DC.W            0,0
-IP_ADDR:                DC.L            $800
-IP_SIZE:                DC.L            $800
-IP_ENTRY:               DC.L            0
-IP_WORK_RAM:            DC.L            0
-SP_SIZE:                DC.L            0
-SP_ENTRY:               DC.L            0
-SP_ADDR:                DC.L            $1000
-SP_SIZE:                DC.L            $7200
-
+DISC_TYPE:      DC.B    'SEGADISCSYSTEM  '
+VOLUME_NAME:    DC.B    'SEGACDGAME     ', 0
+VOLUME_SYSTEM:  DC.W    $100, $1
+SYSTEM_NAME:    DC.B    'SEGACDGAME     ', 0
+SYSTEM_VER:     DC.W    0, 0
+IP_ADDR:        DC.L    $800
+IP_SIZE:        DC.L    $800
+IP_ENTRY:       DC.L    0
+IP_WORK_RAM:    DC.L    0
+SP_SIZE:        DC.L    0
+SP_ENTRY:       DC.L    0
+SP_ADDR:        DC.L    $1000
+SP_SIZE_FINAL:  DC.L    $7200
 
 GAME_HEADER:  
 
-HARDWARE_TYPE:          DC.B            'SEGA MEGA DRIVE    '
-COPYRIGHT:              DC.B            '(C)HARRY CLARK 2024'
-DOMESTIC_NAME:          DC.B            'SEGA CD LOADER                                        '
-OVERSEAS_NAME:          DC.B            'SEGA CD LOADER                                        '
-DISK_ID:                DC.B            'GM XX-XXXX-XX              '
-IO:                     DC.B            'J                          '
-REGION:                 DC.B            'JUE                        '
+HARDWARE_TYPE:  DC.B    'SEGA MEGA DRIVE    '
+COPYRIGHT:      DC.B    '(C)HARRY CLARK 2024'
+DOMESTIC_NAME:  DC.B    'SEGA CD LOADER                                        '
+OVERSEAS_NAME:  DC.B    'SEGA CD LOADER                                        '
+DISK_ID:        DC.B    'GM XX-XXXX-XX              '
+IO:             DC.B    'J                          '
+REGION:         DC.B    'JUE                        '
 
 ;--------------------------------------------------------
 ;                   SUB CPU COUROUTINE
 ;--------------------------------------------------------
 
-ALIGN                   $1000
-INCLUDE                 "BIOS.asm"
-ALIGN                   $8000
+    ALIGN $1000
+    include "BIOS.asm"
+
 
