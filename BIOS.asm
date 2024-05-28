492fbf5952b82b2ed4b60a804e48f7d72f3b95db
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-05-28 15:18:55 +0100

    added sub cpu header and minor pre-req's

--- BIOS.asm
+++ BIOS.asm
@@ -22,3 +22,32 @@ INIT_SYS            EQU         0
 MD_VER              EQU         0
 
 ORG                 EQU         $6000
+
+SUB_HEADER:
+
+NODULE_NAME:            DC.B        'MAIN-SUBCPU', 0
+MODULE_VERSION:         DC.W        0,0
+MODULE_NEXT:            DC.L        0
+MODULE_SIZE:            DC.L        0
+MODULE_START_ADDR:      DC.L        $20
+MODULE_WORK_ADDR:       DC.L        0
+
+SUB_JUMP_TABLE:         DC.W        SP_INIT-SUB_JUMP_TABLE
+                        DC.W        SP_INIT_DRIVE-SUB_JUMP_TABLE
+                        DC.W        SP_IRQ-SUB_JUMP_TABLE
+
+;--------------------------------------------------------
+;              INITIALISE THE STACK POINTER
+;       THIS WORKS UNDEER THE GUISE OF BEING CALLED
+;       ON A SINGLE COUROUTINE AFTER THE INITIAL BOOT
+;--------------------------------------------------------
+
+SP_INIT:
+
+BIOS_MUSIC_STOP
+ANDI.B          #$FA, SUB_MEM               ;; SET SUB CPU MEMORY TO 2M
+BSR             INIT_ISO9660                ;; AFTER WHICH, BRANCH OFF TO INITIALISE THE CD
+
+INIT_ISO9660:                   
+
+    
