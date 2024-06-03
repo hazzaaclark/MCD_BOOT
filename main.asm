673821cf0c7f2f534d9b9eaf60a6fced85781ec4
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-06-03 17:15:48 +0100

    updated multiplicity and stack alignment

--- main.asm
+++ main.asm
@@ -7,16 +7,18 @@
 ;              FUNCTIONALITY OF THE PROGRAM
 ;--------------------------------------------------------
 
-    include "macros.asm"
-    include "BIOS_inc.asm"
+    INCLUDE "macros.asm"
+    INCLUDE "BIOS_inc.asm"
 
-CD_HEADER:
+;---------------------------------------
+;           SEGA MEGA CD HEADER
+;---------------------------------------
 
 DISC_TYPE:      DC.B    'SEGADISCSYSTEM  '
-VOLUME_NAME:    DC.B    'SEGACDGAME     ', 0
+VOLUME_NAME:    DC.B    'SEGACDGAME     ',0
 VOLUME_SYSTEM:  DC.W    $100, $1
-SYSTEM_NAME:    DC.B    'SEGACDGAME     ', 0
-SYSTEM_VER:     DC.W    0, 0
+SYSTEM_NAME:    DC.B    'SEGACDGAME     ',0
+SYSTEM_VER:     DC.W    0,0
 IP_ADDR:        DC.L    $800
 IP_SIZE:        DC.L    $800
 IP_ENTRY:       DC.L    0
@@ -25,16 +27,21 @@ SP_SIZE:        DC.L    0
 SP_ENTRY:       DC.L    0
 SP_ADDR:        DC.L    $1000
 SP_SIZE_FINAL:  DC.L    $7200
+SP_WORK_RAM:    DC.L    0
+                ALIGN   $100 
 
-GAME_HEADER:  
+;---------------------------------------
+;               GAME HEADER
+;---------------------------------------
 
-HARDWARE_TYPE:  DC.B    'SEGA MEGA DRIVE    '
-COPYRIGHT:      DC.B    '(C)HARRY CLARK 2024'
-DOMESTIC_NAME:  DC.B    'SEGA CD LOADER                                        '
-OVERSEAS_NAME:  DC.B    'SEGA CD LOADER                                        '
-DISK_ID:        DC.B    'GM XX-XXXX-XX              '
-IO:             DC.B    'J                          '
-REGION:         DC.B    'JUE                        '
+HARDWARE_TYPE:  DC.B 'SEGA MEGA DRIVE '
+COPYRIGHT:      DC.B '                  HARRY CLARK         '
+NATIVE_NAME:    DC.B 'SEGA CD LOADER                                        '
+OVERSEAS_NAME:  DC.B 'SEGA CD LOADER                                        '
+DISK_ID:        DC.B 'GM XX-XXXX-XX  '
+IO:             DC.B 'J              '
+                ALIGN   $1F0
+REGION:         DC.B 'JUE          '
 
 ;--------------------------------------------------------
 ;                   SUB CPU COUROUTINE
@@ -42,5 +49,6 @@ REGION:         DC.B    'JUE                        '
 
     ALIGN $1000
     include "BIOS.asm"
+    ALIGN $8000
 
 
