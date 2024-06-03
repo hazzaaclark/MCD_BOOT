43ba782a56f32dfe53e057048d9d2eeeeaaeb6b7
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-06-03 11:05:50 +0100

    updated includes and load files

--- operand.asm
+++ operand.asm
@@ -13,6 +13,7 @@
 
     INCLUDE "macros.asm"
     INCLUDE "BIOS_inc.asm"
+    INCLUDE "BIOS.asm"
 
 
 OPERAND_TABLE:
@@ -28,3 +29,15 @@ OPERAND_TABLE:
         BRA.W           OP_STOP_CDDA
         BRA.W           OP_CDDA_FO
         BRA.W           OP_CDDA_SEEK 
+
+OP_GET_WORD_RAM:
+    BSET            #SUB_MEM
+    RTS
+
+OP_LOAD_FILE_ID:
+    MOVEQ           #0, D1
+    MOVE.W          SUB_COMMON_0, D1        ;; GET COMMON FILE ID HEADER INFO
+    LSL.L           #1, D1
+    MOVE.W          (PC, D1.W), D1          ;; SET THE CURRENT INDEX OF THE FILE ID IN THE PC AND MOVE TO D1
+    LEA             (PC, D1.W), A0          ;; LOAD PREVIOUS INTO A0
+    BSR             FIND_FILE               ;; BRANCH TO FIND FILE SUBROUTINE
