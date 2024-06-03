50cc0e90be2d29231e5f4999feca1ee488f03c00
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-06-03 20:58:18 +0100

    updated volume descriptor

--- main.asm
+++ main.asm
@@ -14,19 +14,18 @@
 ;           SEGA MEGA CD HEADER
 ;---------------------------------------
 
-DISC_TYPE:      DC.B    'SEGADISCSYSTEM  '
-VOLUME_NAME:    DC.B    'SEGACDGAME     ',0
+DISC_TYPE:      DC.B    "SEGADISCSYSTEM  "
+VOLUME_NAME:    DC.B    "HARRYS MCD ",0
 VOLUME_SYSTEM:  DC.W    $100, $1
-SYSTEM_NAME:    DC.B    'SEGACDGAME     ',0
+SYSTEM_NAME:    DC.B    "HARRYS MCD ",0
 SYSTEM_VER:     DC.W    0,0
 IP_ADDR:        DC.L    $800
 IP_SIZE:        DC.L    $800
 IP_ENTRY:       DC.L    0
 IP_WORK_RAM:    DC.L    0
-SP_SIZE:        DC.L    0
+SP_SIZE:        DC.L    $7000
 SP_ENTRY:       DC.L    0
 SP_ADDR:        DC.L    $1000
-SP_SIZE_FINAL:  DC.L    $7200
 SP_WORK_RAM:    DC.L    0
                 ALIGN   $100 
 
@@ -34,21 +33,27 @@ SP_WORK_RAM:    DC.L    0
 ;               GAME HEADER
 ;---------------------------------------
 
-HARDWARE_TYPE:  DC.B 'SEGA MEGA DRIVE '
-COPYRIGHT:      DC.B '                  HARRY CLARK         '
-NATIVE_NAME:    DC.B 'SEGA CD LOADER                                        '
-OVERSEAS_NAME:  DC.B 'SEGA CD LOADER                                        '
-DISK_ID:        DC.B 'GM XX-XXXX-XX  '
-IO:             DC.B 'J              '
-                ALIGN   $1F0
-REGION:         DC.B 'JUE          '
+HARDWARE_TYPE:  DC.B "SEGA MEGA DRIVE "
+COPYRIGHT:      DC.B "                  HARRY CLARK  "
+NATIVE_NAME:    DC.B "SEGA CD LOADER                                        "
+OVERSEAS_NAME:  DC.B "SEGA CD LOADER                                        "
+DISK_ID:        DC.B "GM XX-XXXX-XX "
+IO:             DC.B "J             "
+                ALIGN           $1F0
+REGION:         DC.B "J             "
+
+;--------------------------------------------------------
+;           INITIAL PROGRAM SECURITY COUROUTINE
+;--------------------------------------------------------
+
+    INCLUDE "security.asm"
 
 ;--------------------------------------------------------
 ;                   SUB CPU COUROUTINE
 ;--------------------------------------------------------
 
     ALIGN $1000
-    include "BIOS.asm"
+    INCLUDE "BIOS.asm"
     ALIGN $8000
 
 
