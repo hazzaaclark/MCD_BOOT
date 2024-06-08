f7893bdac047fc3f2c990b9a97b0a37b0dc95317
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-06-08 21:18:05 +0100

    added initial vdp defines

--- hello.asm
+++ hello.asm
@@ -13,6 +13,9 @@
 ;       SETUP THE VDP AND DEFINE RELEVANT DIRECTIVES
 ;--------------------------------------------------------
 
+VDP_DATA            EQU     $C00000
+VDP_CTRL            EQU     $C00004
+
 VDP_SETTINGS:
 
     DC.B        $00
@@ -42,3 +45,18 @@ VDP_SETTINGS:
 
 VDP_END:
     EVEN
+
+FONT:
+    INCBIN "Font96.FNT"
+FONT_END:
+
+INIT_VDP:
+    LEA     VDP_SETTINGS, A5
+    MOVE.L  #VDP_END-VDP_SETTINGS, D1       ;; EVALUATE THE SIZE OF THE VDP (PREVENTS OVERFLOW)
+    MOVE.W  (VDP_CTRL), D0                  ;; READ VDP CONTROL STATUS INTO D0
+    MOVE.W  #$00008000, D5
+
+    MOVE.B  (A5)+, D5                       ;; GET NEXT VIDEO CONTROL BYTE
+    MOVE.W  D5, (VDP_CTRL)                  ;; SEND WRITE COMMAND TO VDP
+    ADD.W   #$0100, D5                      ;; POINTER OFFSET TO NEXT VDP COMMAND
+    DBRA    D1, INIT_VDP                    ;; BRANCH BASED OFF A DATA REGISTER DIRECTIVE                     
