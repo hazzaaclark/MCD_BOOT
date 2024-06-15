77cd948f07e55aee4f42c10fb181a8a34f6641b5
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-06-15 16:35:33 +0100

    Update BIOS.asm

--- BIOS.asm
+++ BIOS.asm
@@ -13,28 +13,24 @@
     INCLUDE     "BIOS_inc.asm"
     INCLUDE     "macros.asm"
 
-SP_ROOT     EQU $8000
-
 ;--------------------------------------------------------
 ;       DETERMINE THE VALUES NECESSARY FOR ACCESSING
 ;               SPECIFICS IN THE HARDWARE
 ;--------------------------------------------------------
 
-
 SP_SECTOR:          DC.L        0
 
-                    ORG         SP_START
+MODULE_NAME:            DC.B        'MAIN-SUBCPU',0
+MODULE_VERSION:         DC.W        0, 0
+MODULE_NEXT:            DC.L        0
+MODULE_SIZE:            DC.L        0
+MODULE_START_ADDR:      DC.L        $20
+MODULE_WORK_ADDR:       DC.L        0
 
-                    DC.B        'MAIN-SUBCPU',0
-                    DC.W        0, 0
-                    DC.L        0
-                    DC.L        0
-                    DC.L        $20
-                    DC.L        0
-                    DC.W        SP_INIT-(SP_START+$20)
-                    DC.W        SP_MAIN-(SP_START+$20)
-                    DC.W        SP_IRQ-(SP_START+$20)
-                    DC.W        0
+SUB_JUMP_TABLE:         DC.W        SP_INIT-SUB_JUMP_TABLE
+                        DC.W        SP_MAIN-SUB_JUMP_TABLE
+                        DC.W        SP_IRQ-SUB_JUMP_TABLE
+                        DC.W        0
 
 ;--------------------------------------------------------
 ;              INITIALISE THE STACK POINTER
@@ -43,7 +39,11 @@ SP_SECTOR:          DC.L        0
 ;--------------------------------------------------------
 
 SP_INIT:
-    ANDI.B          #$E2, SUB_WORD_MODE_2_RAM+1               ;; SET SUB CPU MEMORY TO 2M
+    ANDI.B          #$FA, $FF8003               ;; SET SUB CPU MEMORY TO 2M
+    BSR             INIT_ISO9660                ;; AFTER WHICH, BRANCH OFF TO INITIALISE THE CD
+    CLR.B           $FF800F                     ;; CLEAR THE STATUS FLAG TO INITIAL DRIVE   
+    MOVEQ           #0, D0
+    RTS
 
 ;------------------------------------------
 ;       EMPTY FUNCTION THAT DOES NOTHING
@@ -51,20 +51,25 @@ SP_INIT:
 
 SP_IRQ:
     RTS
-
 ;------------------------------------------
 ;  MAIN ROUTINE FOR INITIALISING THE DRIVE
 ;------------------------------------------
-
 SP_MAIN:
-    BIOS_STATUS_TABLE
-    MOVE.W      (A0),D0
-    ANDI.W      #$F000,D0
-    BNE.S     SP_MAIN
+    TST.B   $FF800E
+    BNE     SP_MAIN
+    MOVE.B  #1, $FF800F
 
-    LEA     BIOS_PARAMS(PC), A0
+@LOOP:
+    TST.B   $FF800E
     BEQ     @LOOP
 
+    MOVEQ   #0, D1
+    MOVE.B  $FF800E, D1
+    BTST    #7, D1
+    ANDI.B  #$7F, D1    
+    ADD.W   D1, D1
+    ADD.W   D1, D1
+    JSR     OPERAND_TABLE
     MOVEQ   #0, D0
     MOVE.B  $FF800E, D0  
     ADD.W   D0, D0
@@ -82,15 +87,13 @@ SP_MAIN:
 ;--------------------------------------------------------
 
 INIT_ISO9660:                   
-
     PUSH            D0-D7/A0-A6           ;; STORE ALL RELEVANT REGISTERS FOR CACHE
     MOVE.L          #$10, D0                    ;; START OFFSET FOR PARSING DISC
     MOVE.L          #$2, D1                     ;; OFFSET SIZE  
     LEA.L           SP_SECTOR, A0               ;; EVALUATE THE START OFFSET AT THE EFFECTIVE ADDRESS
     BSR             READ_CD                     ;; BRANCH OFF TO READ THE CONTENTS OF THE CD, BASED ON THE ABOVE PRE-REQ'S
-
-    LEA.L             SP_SECTOR, A0               ;; AFTER WHICH, GET THE POINTER OFFSET CURRENTLY AT A0
-    LEA.L             156(A0), A1     ;; STORE THE ROOT DIRECTORY RECORD FROM A0 AND LOAD IT INTO A1
+    LEA.L           SP_SECTOR, A0               ;; AFTER WHICH, GET THE POINTER OFFSET CURRENTLY AT A0
+    LEA.L           156(A0), A1     ;; STORE THE ROOT DIRECTORY RECORD FROM A0 AND LOAD IT INTO A1
 
     ;; THE FOLLOWING BITSHIFT SECTION SERVES TO
     ;; PROVIDE LIAISSE FOR THE CD DRIVE 
@@ -105,21 +108,18 @@ INIT_ISO9660:
     MOVE.B          8(A1), D0
     LSL.L           #8, D0
     MOVE.B          9(A1), D0
-
     MOVE.L          #$20, D1                    ;; SIZE OF SECTOR OFFSET
     BSR             READ_CD
-
     POP             D0-D7/A0-A6          ;; RESTORE REGISTERS
     RTS
 
 READ_CD:
-
     PUSH            D0-D7/A0-A6                 ;; STORE ALL RELEVANT REGISTERS FOR CACHE
     LEA             BIOS_INIT_STDCALL(PC), A5   ;; INIT AND LOAD THE STDCALL BIOS PARAM (STORES A 32 BIT STATIC LONG)
     MOVE.L          D0, (A5)                    ;; WRITE START OFFSET TO BIOS STDCALL
     MOVE.L          D1, 4(A5)                   ;; WRITE START OFFSET SIZE TO BIOS STDCALL
     MOVE.L          A0, 8(A5)                   ;; WRITE RESULT TO THE RELEVANT ADDRESS
-    MOVEA.L          A5, A0                      ;; STORE RESULT
+    MOVEA.L         A5, A0                      ;; STORE RESULT
     BIOS_CDC_STOP
     BIOS_ROM_READ_SECTOR
 
@@ -152,7 +152,6 @@ FIND_FILE:
     MOVE.B          8(A2), D0
     LSL.L           #8, D0
     MOVE.B          9(A2), D0
-
     MOVE.B	14(A2),D1	
 	LSL.L	#8,D1
     MOVE.B	15(A2),D1
@@ -167,14 +166,11 @@ FIND_FILE:
     POP	    A1/A2/A6		
 	RTS
 
-
 @waitSTAT:
-
     BIOS_CDC_START
     BCS             @waitSTAT
 
 @waitREAD:
-
     BIOS_CDC_READ
     BCC             @waitREAD
 
@@ -182,7 +178,7 @@ FIND_FILE:
     MOVEA.L         8(A5), A0                   ;; GET THE DESTINATION ADDRESS IN RELATION TO THE BITMASK
     LEA             12(A5), A1                  ;; GET HEADER STORE
     BIOS_CDC_WRITE                              ;; WRITE CONTENTS
-    BCC             @WaitTRANSFER               ;; IF NO CONTENTS, BRANCH BACK AND RUN THE SUBROUTINE AGAIN
+    BCC             @waitTRANSFER               ;; IF NO CONTENTS, BRANCH BACK AND RUN THE SUBROUTINE AGAIN
     BIOS_CDC_CALLBACK
     ADDQ.L          #1, (A5)                    ;; INCREMENT WRITE SECTOR
     ADDI.L          #$0800, 8(A5)               ;; INCREMENT DEST REGISTER
@@ -190,13 +186,10 @@ FIND_FILE:
     BNE             @waitSTAT
     POP             D0-D7/A0-A6          ;; RESTORE REGISTERS
     RTS
-
+    
 BIOS_INIT_STDCALL:
-
     DC.L            0, 0, 0, 0, 0
-
 HEADER:
-
     DS.L            32
 ;--------------------------------------------------------
 ;    THE FOLLOWING PERTAINS TOWARDS THE ENCOMPASSING
@@ -204,25 +197,27 @@ HEADER:
 ;                   CD OPERATIONS
 ;--------------------------------------------------------
 
+OPERAND_TABLE:
+
 OPERAND:
     BRA.W           OP_NULL
     BRA.W           OP_GET_WORD_RAM
     BRA.W           OP_LOAD_FILE_NAME
-
+    
 OP_GET_WORD_RAM:
     BSET            #0, $FF8003                       
     RTS
 
 OP_LOAD_FILE_NAME:
-    LEA             @BOOT(PC), A0
+    LEA             @BOOT(PC),A0
     BSR             FIND_FILE
     MOVE.L          #$80000, A0
     BSR             READ_CD
     RTS
 
 @BOOT:
-    DC.B        'game.bin', 0
-    even
+    INCBIN          "SEGA.bin"
+    ALIGN           2
 
 OP_NULL:
     RTS
