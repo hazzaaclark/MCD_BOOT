c551be6d497ad65d239435d5ffca4248031bc412
Harry Clark <107435091+hazzaaclark@users.noreply.github.com>
2024-06-16 10:17:12 +0100

    removed alot of the unecessary subs

--- BIOS.asm
+++ BIOS.asm
@@ -1,11 +1,9 @@
 ;--------------------------------------------------------
 ;           COPYRIGHT (C) HARRY CLARK 2024
 ;--------------------------------------------------------
-;--------------------------------------------------------
 ;           GENERIC SEGA MEGA CD LOADER
 ;--------------------------------------------------------
-;--------------------------------------------------------
-;          THIS FILE PERTAINS TOWARDS THE BIOS
+;          THIS FILE PERTAINS TO THE BIOS
 ;       FUNCTIONALITY - DECLARING AND ACCESSING
 ;     THIS SUB-ROUTINES FOR THE CPU TO BOOT THE DISC
 ;--------------------------------------------------------
@@ -51,9 +49,11 @@ SP_INIT:
 
 SP_IRQ:
     RTS
+
 ;------------------------------------------
 ;  MAIN ROUTINE FOR INITIALISING THE DRIVE
 ;------------------------------------------
+
 SP_MAIN:
     TST.B   $FF800E
     BNE     SP_MAIN
@@ -93,7 +93,7 @@ INIT_ISO9660:
     LEA.L           SP_SECTOR, A0               ;; EVALUATE THE START OFFSET AT THE EFFECTIVE ADDRESS
     BSR             READ_CD                     ;; BRANCH OFF TO READ THE CONTENTS OF THE CD, BASED ON THE ABOVE PRE-REQ'S
     LEA.L           SP_SECTOR, A0               ;; AFTER WHICH, GET THE POINTER OFFSET CURRENTLY AT A0
-    LEA.L           156(A0), A1     ;; STORE THE ROOT DIRECTORY RECORD FROM A0 AND LOAD IT INTO A1
+    LEA.L           156(A0), A1                 ;; STORE THE ROOT DIRECTORY RECORD FROM A0 AND LOAD IT INTO A1
 
     ;; THE FOLLOWING BITSHIFT SECTION SERVES TO
     ;; PROVIDE LIAISSE FOR THE CD DRIVE 
@@ -127,72 +127,13 @@ FIND_FILE:
     PUSH              A1/A2/A6             ;; STORE USED REGISTERS FOR READING CD FILE
     LEA.L             SP_SECTOR, A1               ;; LOAD THE SECTOR OFFSET INTO A1
 
-@readFILENAME_START:
-    MOVEA.L         A0, A6                      ;; STORE FILENAME POINTER
-    MOVE.B          (A6)+,D0                   ;; ADD THE CORRESPONDENCE FROM A6 INTO D0 (READ CHAR FROM FILE)
 
-@findFIRST_CHAR:
-    MOVEA.L         A1,A2                      ;; STORE SECTOR BUFFER POINTER BEFORE COMPARISON
-    CMP.B           (A1)+,D0                   ;; COMPARE THE RESPECTIVE CHAR DIRECTIVE
-    BNE.B           @findFIRST_CHAR
-
-@checkCHARS:
-    MOVE.B          (A6)+,D0                    ;; INCREMENT INFO BYTES CACHE BY THE CONTENTS LOADED FROM D0
-    BEQ             @GETINFO                    ;; IF SUCCESSFUL, BRANCH TO THE LOOP EQUAL TO IT'S RETURN TYPE  
-    CMP.B           (A1)+,D0    
-    BNE.B           @readFILENAME_START
-    BRA.B           @checkCHARS
-
-@GETINFO:
-    SUB.L           #33, A2
-    MOVE.B          6(A2), D0
-    LSL.L           #8, D0
-    MOVE.B          7(A2), D0
-    LSL.L           #8, D0
-    MOVE.B          8(A2), D0
-    LSL.L           #8, D0
-    MOVE.B          9(A2), D0
-    MOVE.B	14(A2),D1	
-	LSL.L	#8,D1
-    MOVE.B	15(A2),D1
-    LSL.L	#8,D1
-	MOVE.B	16(A2),D1
-	LSL.L	#8,D1
-	MOVE.B	17(A2),D1
-						
-	LSR.L	#8,D1
-	LSR.L	#3,D1
-	
-    POP	    A1/A2/A6		
-	RTS
-
-@waitSTAT:
-    BIOS_CDC_START
-    BCS             @waitSTAT
-
-@waitREAD:
-    BIOS_CDC_READ
-    BCC             @waitREAD
-
-@WaitTRANSFER:
-    MOVEA.L         8(A5), A0                   ;; GET THE DESTINATION ADDRESS IN RELATION TO THE BITMASK
-    LEA             12(A5), A1                  ;; GET HEADER STORE
-    BIOS_CDC_WRITE                              ;; WRITE CONTENTS
-    BCC             @waitTRANSFER               ;; IF NO CONTENTS, BRANCH BACK AND RUN THE SUBROUTINE AGAIN
-    BIOS_CDC_CALLBACK
-    ADDQ.L          #1, (A5)                    ;; INCREMENT WRITE SECTOR
-    ADDI.L          #$0800, 8(A5)               ;; INCREMENT DEST REGISTER
-    SUBQ.L          #1, 4(A5)
-    BNE             @waitSTAT
-    POP             D0-D7/A0-A6          ;; RESTORE REGISTERS
-    RTS
-    
 BIOS_INIT_STDCALL:
     DC.L            0, 0, 0, 0, 0
 HEADER:
     DS.L            32
 ;--------------------------------------------------------
-;    THE FOLLOWING PERTAINS TOWARDS THE ENCOMPASSING
+;    THE FOLLOWING PERTAINS TO THE ENCOMPASSING
 ;   LOGIC SURROUNDING THE OPERANDS USED FOR RELEVANT
 ;                   CD OPERATIONS
 ;--------------------------------------------------------
@@ -216,8 +157,8 @@ OP_LOAD_FILE_NAME:
     RTS
 
 @BOOT:
-    INCBIN          "SEGA.bin"
-    ALIGN           2
+    INCBIN      "SEGA.bin"
+    ALIGN       2
 
 OP_NULL:
     RTS
